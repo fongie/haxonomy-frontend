efa6d43a70ca7a3ca69588fd1d856f7f8f88ac21
Anders <andpet52@kth.se>
2019-04-27 18:12:23 +0200

    återställde tool designen

--- src/pages/Tool.js
+++ src/pages/Tool.js
@@ -4,6 +4,9 @@ import {Row, Col} from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
+import CardColumns from 'react-bootstrap/CardColumns'
+import CardDeck from 'react-bootstrap/CardDeck'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 /**
  * UI for the tool that guides the user through the search for web vulnerabilities
@@ -20,32 +23,36 @@ class Tool extends Component{
 
     render() {
         return (
-            <div>
-                <Row>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Body>
-                                <Card.Title>Action</Card.Title>
-                                <Card.Text>
-                                    Describes a term that the user should look for...
-                                </Card.Text>
-                                <Button variant="primary">YES</Button>
-                                <Button variant="primary">NO</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Header>Featured</Card.Header>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                            </ListGroup>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
+            <CardDeck>
+                <Card style={{width: '45vw'}}>
+                    <Card.Body>
+                        <Card.Title>Next action</Card.Title>
+                        <Card.Text>
+                            Does the site have HTTPS
+                        </Card.Text>
+                        <Button variant="primary" style={{margin:'0.5em'}}>YES</Button>
+                        <Button variant="primary">NO</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{width: '18rem'}}>
+                    <Card.Header>Reports</Card.Header>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>First report</ListGroup.Item>
+                        <ListGroup.Item>Second report</ListGroup.Item>
+                        <ListGroup.Item>Third report</ListGroup.Item>
+                    </ListGroup>
+                </Card>
+                <Card>
+                    <p>Injection</p>
+                    <ProgressBar striped variant="success" now={40}/>
+                    <p>Broken Authentication</p>
+                    <ProgressBar striped variant="info" now={20}/>
+                    <p>Sensitive Data Exposure</p>
+                    <ProgressBar striped variant="warning" now={60}/>
+                    <p>XML External Entities</p>
+                    <ProgressBar striped variant="danger" now={80}/>
+                </Card>
+            </CardDeck>
         )
     }
 
